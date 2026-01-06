b91e8622a16e6d00f14b6b8847a1db46632e07af
Jacob Hauck <hauckjp@gmail.com>
2026-01-06 15:04:57 -0500

    Move 1D data generation to data/pd1d, and add .gitignore for datasets

---
+++ data/pd1d/OLPDGenerate1.m
@@ -0,0 +1,88 @@
+% Generates peridynamics operator learning datasets of type 1
+
+% ===== Configuration =====
+
+% Output file
+fileName = "test2.ol.h5";
+
+% Whether to show an example before generating the dataset
+showExample = false;
+
+% Size of dataset
+numSamples = 500;
+
+% Spatial interval
+x0 = 0;
+xn = 1;
+numCells = 512;
+
+% Temporal interval
+t0 = 0;
+tn = 1;
+dt = 0.001;
+
+% Order of influence function
+omegaOrder = 0;
+
+% Peridynamic horizon
+delta = 0.02;
+
+% Input GRF parameters
+grfNumModes = 64;
+grfMean = 0;
+grfGamma = 1;
+grfTau = 1;
+grfSigma= 0.2;
+grfType = "neumann";
+
+
+% ===== Dataset generation =====
+
+% Create PDProblem
+problem = PDProblem();
+problem.setUniformNodes(x0, xn, numCells);
+problem.setUniformTimeSteps(t0, tn, dt);
+problem.setInfluenceFunction(omegaOrder);
+problem.delta = delta;
+
+zero = zeros(size(problem.x));
+problem.v0 = @(x) zero;
+problem.bfunc = @(x, t) zero;
+
+% Create solver
+solver = PDSolver(problem);
+
+% Create saver
+saver = OLPDSaver1(problem, numSamples);
+
+% Show an example, if requested
+if showExample
+    u0ref = GRF(grfNumModes, grfMean, grfGamma, grfTau, grfSigma, grfType);
+    problem.u0 = @(x) u0ref((x - x0) / (xn - x0));
+    exampleSaver = PDFullSaver(problem);
+    solver.solve(@exampleSaver.stepCallback);
+    
+    h = animatedline;
+    xlabel("x");
+    ylabel("u(x, t)");
+    ylim([min(min(exampleSaver.u)), max(max(exampleSaver.u))]);
+    for k = 1:length(problem.t)
+        clearpoints(h);
+        addpoints(h, problem.x, exampleSaver.u(:, k));
+        drawnow;
+    end
+end
+
+% Generate dataset
+prog = waitbar(0, "Starting dataset generation");
+for i = 1:numSamples
+    u0ref = GRF(grfNumModes, grfMean, grfGamma, grfTau, grfSigma, grfType);
+    problem.u0 = @(x) u0ref((x - x0) / (xn - x0));
+    saver.activeSample = i;
+    solver.solve(@saver.stepCallback);
+    waitbar(i / numSamples, prog, sprintf("Generated %d / %d", i, numSamples));
+end
+
+waitbar(1.0, prog, "Saving...");
+saver.writeDataset(fileName);
+close(prog);
--- data/pd1d/OLPDSaver1.m
+++ data/pd1d/OLPDSaver1.m
@@ -6,6 +6,10 @@ classdef OLPDSaver1 < handle
     %
 
     properties
+        % This dataset uses the same PDProblem for all data points, so
+        % store the problem here
+        problem
+
         % int, which sample in the dataset is being written
         activeSample = 1
 
@@ -37,7 +41,6 @@ classdef OLPDSaver1 < handle
             % numSamples: Size of the operator learning dataset
 
             if nargin > 0
-                self.numTimeSteps = length(problem.t);
                 self.allocate(problem, numSamples);
             else
                 warning("Creating PDFullSaver without problem information is slow. Call allocate(problem) to pre-allocate output variables.");
@@ -54,6 +57,8 @@ classdef OLPDSaver1 < handle
             % ----------
             % problem: PDProblem for which to allocate memory
             
+            self.problem = problem;
+            self.numTimeSteps = length(problem.t);
             self.u0 = zeros(numSamples, problem.numNodes());
             self.un = zeros(numSamples, problem.numNodes());
         end
@@ -76,6 +81,49 @@ classdef OLPDSaver1 < handle
             if self.showProgress
                 fprintf("Simulation step %d\n", k);
             end
+        end
+
+        function writeDataset(self, fileName)
+            % Writes the saved data to an operator learning dataset file
+            %
+            % Parameters
+            % ----------
+            % fileName: Name of the output dataset file (.ol.h5)
+            %
+            % Overwrites fileName file with an operator learning dataset
+            % containing the data saved on this object
+            if isfile(fileName)
+                delete(fileName);
+            end
+            
+            % ***NOTE***: Make sure to write *all* arrays backwards because
+            % MATLAB uses FORTRAN ordering, but NumPy uses C ordering
+
+            % Create discretization information (x and y)
+            numNodes = self.problem.numNodes();
+            h5create(fileName, '/x/1', [1, numNodes], 'Datatype', 'single');
+            h5write(fileName, '/x/1', single(self.problem.x'));
+            h5writeatt(fileName, '/x/1', 'id', 1);
+            
+            h5create(fileName, '/y/1', [1, numNodes], 'Datatype', 'single');
+            h5write(fileName, '/y/1', single(self.problem.x'));
+            h5writeatt(fileName, '/y/1', 'id', 1);
+
+            % Save u and v
+            numSamples = size(self.u0, 1);
+            indices = int32(0 : (numSamples-1));
+            
+            h5create(fileName, '/u/1/u', [1, numNodes, numSamples], 'Datatype', 'single');
+            h5write(fileName, '/u/1/u', single(reshape(self.u0', 1, numNodes, numSamples)));
+            h5create(fileName, '/u/1/indices', numSamples, 'Datatype', 'int32');
+            h5write(fileName, '/u/1/indices', indices);
+            h5writeatt(fileName, '/u/1', 'disc_id', 1);
+
+            h5create(fileName, '/v/1/v', [1, numNodes, numSamples], 'Datatype', 'single');
+            h5write(fileName, '/v/1/v', single(reshape(self.un', 1, numNodes, numSamples)));
+            h5create(fileName, '/v/1/indices', numSamples, 'Datatype', 'int32');
+            h5write(fileName, '/v/1/indices', indices);
+            h5writeatt(fileName, '/v/1', 'disc_id', 1);
         end
     end
 end
