cb934380e2ae8e6f1e48ab8814019e50bdc82ee3
Jacob Hauck <hauckjp@gmail.com>
2026-02-20 10:53:19 -0500

    Print out metrics add the end of pd2d run

--- experiments/pd2d/pd2d.py
+++ experiments/pd2d/pd2d.py
@@ -74,12 +74,20 @@ class PD2DTraining(mlx.WandBExperiment):
             trainer.apply_model = lambda u, x, y: trainer.model(u)
 
         trainer.train(epochs=config['training']['epochs'])
-        losses, _ = trainer.evaluate(('train', 'test'))
+        losses, metrics = trainer.evaluate(('train', 'test'))
 
         for dataset, dataset_losses in losses.items():
             print(f'===== Loss for dataset: "{dataset}" =====')
-            for loss_name, losses in dataset_losses.items():
+            for loss_name, loss in dataset_losses.items():
                 print(f'    === Loss: {loss_name} ===')
-                print(f'    Mean: {losses.mean().item():.05f}')
-                print(f'    Median: {losses.median().item():.05f}')
-                print(f'    Std.: {losses.std().item():.05f}')
+                print(f'    Mean: {loss.mean().item():.05f}')
+                print(f'    Median: {loss.median().item():.05f}')
+                print(f'    Std.: {loss.std().item():.05f}')
+
+        for dataset, dataset_metrics in metrics.items():
+            print(f'===== Metric for dataset: "{dataset}" =====')
+            for metric_name, metric in dataset_metrics.items():
+                print(f'    === Loss: {metric_name} ===')
+                print(f'    Mean: {metric.mean().item():.05f}')
+                print(f'    Median: {metric.median().item():.05f}')
+                print(f'    Std.: {metric.std().item():.05f}')
