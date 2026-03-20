98ec63e58b2912a66e47508551567beef01cf7d7
Jacob Hauck <hauckjp@gmail.com>
2026-03-20 13:41:00 -0400

    Add NormalizedOLDataset class

--- modules/data.py
+++ modules/data.py
@@ -1,4 +1,5 @@
 import torch
+import torch.utils.data
 from operatorlearning.data import OLDataset
 
 
@@ -31,3 +32,50 @@ def pd2d_import_dataset(dataset_in, dataset_out, nx, ny):
 
     disc = torch.zeros(len(u), dtype=torch.long)
     OLDataset.write(u, [x], v, [y], dataset_out, u_disc=disc, v_disc=disc)
+
+
+class NormalizedOLDataset(torch.utils.data.Dataset):
+    def __init__(self, base_dataset):
+        super().__init__()
+        assert len(base_dataset.x) == 1, \
+            'NormalizedOLDataset only supports single-representation OLDatasets'
+
+        self.base_dataset = base_dataset
+
+        u0, _, v0, _ = base_dataset[0]
+        all_u = torch.empty((len(base_dataset), *u0.shape), device=u0.device)
+        all_v = torch.empty((len(base_dataset), *v0.shape), device=v0.device)
+        for i, data in base_dataset:
+            all_u[i] = data[0]
+            all_v[i] = data[2]
+
+        print('Normalizing OLDataset')
+        self.u_mean = torch.mean(all_u.view(-1, all_u.shape[-1]), dim=0)
+        self.u_mean = self.u_mean.reshape((1,) * len(u0.shape) + (-1,))
+        self.u_std = torch.mean(all_u.view(-1, all_u.shape[-1]), dim=0)
+        self.u_std = self.u_std.reshape((1,) * len(u0.shape) + (-1,))
+        self.v_mean = torch.mean(all_v.view(-1, all_v.shape[-1]), dim=0)
+        self.v_mean = self.v_mean.reshape((1,) * len(v0.shape) + (-1,))
+        self.v_std = torch.mean(all_v.view(-1, all_v.shape[-1]), dim=0)
+        self.v_std = self.v_std.reshape((1,) * len(v0.shape) + (-1,))
+
+    def __len__(self):
+        return len(self.base_dataset)
+
+    def normalize_u(self, u):
+        return (u - self.u_mean) / self.u_std
+
+    def normalize_v(self, v):
+        return  (v - self.v_mean) / self.v_std
+
+    def denormalize_u(self, u_z):
+        return self.u_mean + self.u_std * u_z
+
+    def denormalize_v(self, v_z):
+        return self.v_mean + self.v_std * v_z
+
+    def __getitem__(self, item):
+        u, x, v, y = self.base_dataset[item]
+        u_z = self.normalize_u(u)
+        v_z = self.normalize_v(v)
+        return u_z, x, v_z, y
