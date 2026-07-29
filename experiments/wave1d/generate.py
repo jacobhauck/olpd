bb239777c1ace403201e7437b1581ac6dd4fb8e9
Jacob Hauck <hauckjp@gmail.com>
2026-07-29 13:36:04 -0400

    Update heat1d and wave1d to final version

--- experiments/wave1d/generate.py
+++ experiments/wave1d/generate.py
@@ -1,34 +1,89 @@
 import mlx
 import torch
 import tqdm
-from operatorlearning.data import OLDataset
+from operatorlearning.data import OLDataset, OLDatasetLibrary
 
 
 @mlx.experiment
 def generate(config, name, group=None):
-    n = torch.arange(1, config['num_modes'] + 1).to(torch.float)
-    x = torch.linspace(config['x0'], config['x1'], config['mesh_size'])
+    if 'seed' in config:
+        torch.random.manual_seed(config['seed'])
+    n = torch.arange(0, config['num_modes'] + 1).to(torch.float)  # (num_modes + 1,)
+    x = torch.linspace(config['x0'], config['x1'], config['mesh_size'])  # (mesh_size,)
     x += (x[1] - x[0]) / 2
     size = config['x1'] - config['x0']
     sq_lam = config['alpha'] / (config['beta'] + (n*torch.pi/size)**2) ** (config['gamma'] / 2)
-    basis = sq_lam[None] * torch.cos(n[None] * x[:, None] * torch.pi / size)
-    basis_v = basis * torch.cos(n[None] * torch.pi * config['c'] * config['t_final'] / size)
+    # (num_modes + 1,)
 
-    for split_name, split_size in config['splits'].items():
-        print(f'Generating split {split_name}')
-        all_u = []
-        all_v = []
+    c = config['c']
+    t = config['t_final']
+    pi = torch.pi
 
-        for _ in tqdm.tqdm(range(split_size)):
-            coef = torch.randn((1, len(n)))
-            all_u.append((coef * basis).sum(dim=1)[:, None])
-            all_v.append((coef * basis_v).sum(dim=1)[:, None])
+    basis_c = sq_lam[None] * torch.cos(n[None] * x[:, None] * 2 * pi / size) * (2/size)**.5
+    basis_c[:, 0] /= 2**.5
+    # (mesh_size, num_modes + 1)
+    basis_s = sq_lam[None, 1:] * torch.sin(n[None, 1:] * x[:, None] * 2 * pi / size) * (2/size)**.5
+    # (mesh_size, num_modes)
 
-        output_file = 'data/wave1d/' + split_name + str(config['dataset_id']) + '.ol.h5'
-        print(f'Saving dataset split at {output_file}')
-        OLDataset.write(
-            all_u, [x[:, None]], all_v, [x[:, None]],
-            file_name=output_file,
-            u_disc=torch.zeros(split_size, dtype=torch.long),
-            v_disc=torch.zeros(split_size, dtype=torch.long)
-        )
+    data_lib = OLDatasetLibrary('wave1d')
+    dataset_id = data_lib.create_dataset(
+        c=config['c'],
+        x0=config['x0'],
+        x1=config['x1'],
+        t_final=config['t_final'],
+        mesh_size=config['mesh_size'],
+        alpha=config['alpha'],
+        beta=config['beta'],
+        gamma=config['gamma'],
+        num_modes=config['num_modes']
+    )
+
+    try:
+        for split_name, split_size in config['splits'].items():
+            print(f'Generating split {split_name}')
+            all_u = []
+            all_v = []
+
+            for _ in tqdm.tqdm(range(split_size)):
+                coef_c_1 = torch.randn((1, len(n)))  # (1, num_modes + 1)
+                coef_s_1 = torch.randn((1, len(n) - 1))  # (1, num_modes)
+                coef_c_2 = torch.randn((1, len(n)))  # (1, num_modes + 1)
+                coef_s_2 = torch.randn((1, len(n) - 1))  # (1, num_modes)
+                u_1 = (coef_c_1 * basis_c).sum(dim=1) + (coef_s_1 * basis_s).sum(dim=1)  # (mesh_size,)
+                u_2 = (coef_c_2 * basis_c).sum(dim=1) + (coef_s_2 * basis_s).sum(dim=1)  # (mesh_size,)
+                all_u.append(torch.stack([u_1, u_2], dim=1))  # (mesh_size, 2)
+
+                v_coef_c_1 = coef_c_1.clone()
+                v_coef_c_1[:, 1:] *= torch.cos(n[None, 1:] * (2*pi*c*t/size))
+                v_coef_c_1[:, 0] += t * coef_c_2[:, 0]
+                v_coef_c_1[:, 1:] += (size/(n[None, 1:] * 2*pi*c)) * torch.sin(n[None, 1:] * (2*pi*c*t/size)) * coef_c_2[:, 1:]
+
+                v_coef_s_1 = coef_s_1.clone()
+                v_coef_s_1 *= torch.cos(n[None, 1:] * (2*pi*c*t/size))
+                v_coef_s_1 += (size/(n[None, 1:] * 2*pi*c)) * torch.sin(n[None, 1:] * (2*pi*c*t/size)) * coef_s_2
+
+                v_coef_c_2 = coef_c_2.clone()
+                v_coef_c_2[:, 1:] *= torch.cos(n[None, 1:] * (2*pi*c*t/size))
+                v_coef_c_2[:, 1:] -= ((n[None, 1:] * 2*pi*c)/size) * torch.sin(n[None, 1:] * (2*pi*c*t/size)) * coef_c_1[:, 1:]
+
+                v_coef_s_2 = coef_s_2.clone()
+                v_coef_s_2 *= torch.cos(n[None, 1:] * (2*pi*c*t/size))
+                v_coef_s_2 -= ((n[None, 1:] * 2*pi*c)/size) * torch.sin(n[None, 1:] * (2*pi*c*t/size)) * coef_s_1
+
+                v_1 = (v_coef_c_1 * basis_c).sum(dim=1) + (v_coef_s_1 * basis_s).sum(dim=1)  # (mesh_size,)
+                v_2 = (v_coef_c_2 * basis_c).sum(dim=1) + (v_coef_s_2 * basis_s).sum(dim=1)  # (mesh_size,)
+                all_v.append(torch.stack([v_1, v_2], dim=1))  # (mesh_size, 2)
+
+            output_file = data_lib.dataset_path(split_name, dataset_id)
+            print(f'Saving dataset split at {output_file}')
+            OLDataset.write(
+                all_u, [x[:, None]], all_v, [x[:, None]],
+                file_name=output_file,
+                u_disc=torch.zeros(split_size, dtype=torch.long),
+                v_disc=torch.zeros(split_size, dtype=torch.long)
+            )
+
+    except Exception as e:
+        print('Aborting dataset generation due to an error')
+        data_lib.delete_dataset(dataset_id)
+        raise e
--- experiments/wave1d/visualize.py
+++ experiments/wave1d/visualize.py
@@ -1,31 +1,47 @@
 import mlx
 import mlx.utils
-from operatorlearning.data import OLDataset
+from operatorlearning.data import OLDataset, OLDatasetLibrary
 import matplotlib.pyplot as plt
 import os
 
 
 @mlx.experiment
 def visualize(config, name, group=None):
-    dataset = OLDataset(config['dataset'], stream_uv=False)
+    data_lib = OLDatasetLibrary('wave1d')
+    dataset = OLDataset(data_lib.dataset_path(config['split'], config['dataset_id']), stream_uv=False)
 
-    output_dir = os.path.join('results', name)
+    output_dir = os.path.join('results', name, str(config['dataset_id']))
     os.makedirs(output_dir, exist_ok=True)
 
     for i in mlx.utils.subset_indices(config, dataset):
         u, x, v, y = dataset[i]
-        fig, axes = plt.subplots(1, 2, sharey=True, figsize=(16, 6))
-        axes[0].plot(x, u)
-        axes[0].set_title(f'Initial displacement ({i})')
-        axes[0].set_xlabel('$x$')
-        axes[0].set_ylabel('$u(x, 0)$')
-        axes[1].plot(y, v)
-        axes[1].set_title(f'Final displacement ({i})')
-        axes[1].set_xlabel('$x$')
-        axes[1].set_ylabel('$u(x, T)$')
+        fig, axes = plt.subplots(2, 2, figsize=(16, 6))
+        axes[0][0].plot(x, u[:, 0], label='$u_1$')
+        axes[0][0].set_title(f'Initial displacement ({i})')
+        axes[0][0].set_xlabel('$x$')
+        axes[0][0].set_ylabel('$u(x, 0)$')
+        axes[0][0].legend()
+
+        axes[1][0].plot(x, u[:, 1], label='$u_2$')
+        axes[1][0].set_title(f'Initial velocity ({i})')
+        axes[1][0].set_xlabel('$x$')
+        axes[1][0].set_ylabel('$u_t(x, 0)$')
+        axes[1][0].legend()
+
+        axes[0][1].plot(y, v[:, 0], label='$v_1$')
+        axes[0][1].set_title(f'Final displacement ({i})')
+        axes[0][1].set_xlabel('$x$')
+        axes[0][1].set_ylabel('$v(x, 0)$')
+        axes[0][1].legend()
+
+        axes[1][1].plot(y, v[:, 1], label='$v_2$')
+        axes[1][1].set_title(f'Final velocity ({i})')
+        axes[1][1].set_xlabel('$x$')
+        axes[1][1].set_ylabel('$v_t(x, 0)$')
+        axes[1][1].legend()
 
         plt.savefig(
-            os.path.join(output_dir, str(i) + '.png'),
+            os.path.join(output_dir, config['split'] + '-' + str(i) + '.png'),
             bbox_inches='tight'
         )
 
