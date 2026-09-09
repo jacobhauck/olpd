299d4db263fc397670803370f30a266604161474
Jacob Hauck <hauckjp@gmail.com>
2026-09-09 13:32:23 -0500

    Add script to generate Table 1

---
+++ experiments/ad2d/table1/table1.py
@@ -0,0 +1,28 @@
+import mlx
+import os
+import csv
+import tqdm
+from ..ad2d import AD2DTrainer
+from operatorlearning.data import OLDatasetLibrary
+
+
+@mlx.experiment
+def make_table(config, name, group=None):
+    lib = OLDatasetLibrary('ad2d')
+    k_index = {1: 0, 2: 1, 3: 2, 4: 3, 5: 4}
+    rows = [['Model', 1, 5, 25, 125, 625]]
+    for model, ids in config['runs_by_model'].items():
+        row = [model, 0, 0, 0, 0, 0]
+        print(f'Evaluating model: {model}')
+        for run_id in tqdm.tqdm(ids):
+            run = mlx.load_run(run_id)
+            _, dataset_id, _ = lib.parse_path(run.config['data']['train']['file_name'])
+            trainer = AD2DTrainer(run.config, run)
+            _, metrics = trainer.evaluate([config['split']])
+            metric = metrics[config['split']][config['metric']]
+            row[k_index[dataset_id] + 1] = float(metric.mean())
+        rows.append(row)
+
+    output_file = os.path.join(mlx.results_dir(name), 'table1.csv')
+    with open(output_file, 'w', newline='') as f:
+        csv.writer(f).writerows(rows)
